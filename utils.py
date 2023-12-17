8be3a33203c8120ee3b65c6e373f8330db59d375
Goggy744 <hugo.barthelemy.001@student.uni.lu>
2023-12-17 14:21:23 +0100

    Added utils.py, justified userData with absolute timestamp and new requirements.txt

--- utils.py
+++ utils.py
@@ -1,12 +1,34 @@
 import pandas as pd
+import datetime
 
 PATH = "COLLECTED-DATA/"
+
 def justify_time(filename):
   """
   Function that take a file with relative timestamp and return a file with absolute timestamp
   """
   
-  f = pd.read_csv(PATH + filename)
+  data = pd.read_csv(PATH + filename+'.csv')
+  relative_to = data.columns[3]
+  relative_to = relative_to.split('(')[1].split(')')[0]
   
-  print(f.columns)
+  date, time = relative_to.split(" ")
+  year, month, day = date.split('/')
+  milliseconds = time.split('.')[1]
+  hours, minutes, seconds = time.split('.')[0].split(':')
+  year, month, day, hours, minutes, seconds, milliseconds = int(year), int(month), int(day), int(hours), int(minutes), int(seconds), int(milliseconds)
+  
+  relative_to = datetime.datetime(year, month, day, hours, minutes, seconds, milliseconds).strftime('%s')
+  
+  justified_data = data.copy()
+  
+  col_name = justified_data.columns[3]
+  for i in range(0, justified_data.shape[0]):
+    current_value = justified_data.iloc[i,3]
+    new_value = current_value + int(relative_to)
+    
+    justified_data[col_name] = justified_data[col_name].replace([current_value], new_value)
+  
+  
+  justified_data.to_csv(PATH+filename+'_justified_data.csv', index=False)
   
